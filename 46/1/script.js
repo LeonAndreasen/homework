5a4f5d654c713a7da5262727daf4282c1542c503
Leon <leon.andreasen@skola.botkyrka.se>
2020-11-15 14:47:23 +0100

    uppgift1

---
+++ 46/1/script.js
@@ -0,0 +1,8 @@
+const x = 3
+const y = 8
+
+const msg = 'Sum of ' + x + ' and ' + y + ' equals ' + (x+y) 
+console.log(msg)
+
+const message = `Sum of ${x} and ${y} equals ${x + y}` //kortare och lättare att skriva med interpolation
+console.log(message)
