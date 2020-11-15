bb4141632d80a8159f583b0d1b0a953716d5386d
Leon <leon.andreasen@skola.botkyrka.se>
2020-11-15 14:33:28 +0100

    uppgift3

---
+++ 46/3/script.js
@@ -0,0 +1,7 @@
+const animal ={species: "Reptile", name: "Crocodile" ,Age: 83,}
+console.log(animal.Age)
+console.log(animal.Age + 1) // ger 83 + 1
+
+
+const Age = animal.Age + 1 
+console.log(Age)
