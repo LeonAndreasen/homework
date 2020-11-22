26298c88f68348232249a26b6ce6bf81f504e005
Leon_ <65647418+LeonAndreasen@users.noreply.github.com>
2020-11-23 00:26:16 +0100

    Add files via upload

---
+++ 47/1/script.js
@@ -0,0 +1,18 @@
+
+// låt i vara 0
+
+// while i < 6
+//      logga ut i
+//      öka i med 1
+
+// logga ut "Färdig!"
+
+let i = 0
+
+function count() {
+    while (i < 6)
+    
+console.log(i++)
+console.log("färdig!")
+}
+console.log(count())
