0d5e9252e7b23d6303719026ea43a24653e269cb
Leon <leon.andreasen@skola.botkyrka.se>
2020-11-29 13:35:33 +0100

    2

--- 48/1/script.js
+++ 48/1/script.js
@@ -1,3 +1,9 @@
 // Deklarera en variabel name och ge den värdet av ditt namn
 
 // Skriv en for-loop som loggar ut "Hejsan <ditt namn här>!!" 500 gånger
+
+let name = "Leon"
+
+for (let greetings = 0; greetings < 500; greetings++) {
+    console.log("Hejsan " + name)
+    }
