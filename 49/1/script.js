1c648f7f2835398e4c47511639194a866d6ff183
Leon <leon.andreasen@skola.botkyrka.se>
2020-12-07 00:24:16 +0100

    uppgift1

---
+++ 49/1/script.js
@@ -0,0 +1,26 @@
+//Upp och nedvänd triangel
+let str = ''
+for (let i = 10; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+        str = str + '*'
+    }
+    str = str + '\n'}
+    console.log(str)
+
+    //mellanslag efter varje
+    let a = ''
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < i; j++) {
+        a = a + ' *'
+    }
+    a = a + '\n'
+}
+console.log(a)
+
+let f = ''
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < i; j++) {
+        f = f + '*'
+    }
+    f = f + '\n' 
+}
